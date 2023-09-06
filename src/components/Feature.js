30ee0d04c699ba6a3929b7b957a1e9f7e8dd2cfd
adilabbasi-786 <m.adilabbasi786@gmail.com>
2023-09-06 14:17:26 +0500

    feature category from backend

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import feature1 from "../assests/feature/feature_1.png";
 import feature2 from "../assests/feature/feature_2.png";
 import feature3 from "../assests/feature/feature_3.png";
 import feature4 from "../assests/feature/feature_4.png";
 
 function Feature() {
+  const [category, setCategory] = useState([]);
+
+  const getData = async () => {
+    let req = await fetch("http://localhost:1337/api/categories?populate=*");
+    let res = await req.json();
+    setCategory(res.data);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div>
       <section class="feature_part padding_top">
@@ -17,46 +27,24 @@ function Feature() {
             </div>
           </div>
           <div class="row align-items-center justify-content-between">
-            <div class="col-lg-7 col-sm-6">
-              <div class="single_feature_post_text">
-                <p>Premium Quality</p>
-                <h3>Latest foam Sofa</h3>
-                <a href="#" class="feature_btn">
-                  EXPLORE NOW <i class="fas fa-play"></i>
-                </a>
-                <img src={feature1} alt="" />
+            {category.map((item, index) => (
+              <div
+                key={index}
+                className={`col-lg-${index % 2 === 0 ? "7" : "5"} col-sm-6`}
+              >
+                <div class="single_feature_post_text">
+                  <p>Premium Quality</p>
+                  <h3>Latest foam {item?.attributes?.name}</h3>
+                  <a href="#" class="feature_btn">
+                    EXPLORE NOW <i class="fas fa-play"></i>
+                  </a>
+                  <img
+                    src={`http://localhost:1337${item?.attributes?.img?.data?.attributes?.url}`}
+                    alt=""
+                  />
+                </div>
               </div>
-            </div>
-            <div class="col-lg-5 col-sm-6">
-              <div class="single_feature_post_text">
-                <p>Premium Quality</p>
-                <h3>Latest foam Sofa</h3>
-                <a href="#" class="feature_btn">
-                  EXPLORE NOW <i class="fas fa-play"></i>
-                </a>
-                <img src={feature2} alt="" />
-              </div>
-            </div>
-            <div class="col-lg-5 col-sm-6">
-              <div class="single_feature_post_text">
-                <p>Premium Quality</p>
-                <h3>Latest foam Sofa</h3>
-                <a href="#" class="feature_btn">
-                  EXPLORE NOW <i class="fas fa-play"></i>
-                </a>
-                <img src={feature3} alt="" />
-              </div>
-            </div>
-            <div class="col-lg-7 col-sm-6">
-              <div class="single_feature_post_text">
-                <p>Premium Quality</p>
-                <h3>Latest foam Sofa</h3>
-                <a href="#" class="feature_btn">
-                  EXPLORE NOW <i class="fas fa-play"></i>
-                </a>
-                <img src={feature4} alt="" />
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </section>
